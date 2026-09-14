4403d6feacd6492b9c3b05fb5a0f87ce137023a5
Eric <dekribeiro7@gmail.com>
2026-09-14 00:20:31 -0300

    fix the exercise 5.13

--- Fad/Chapter5-Ex.lean
+++ Fad/Chapter5-Ex.lean
@@ -263,13 +263,6 @@ def split [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
     else (acc.1, x :: acc.2.2, acc.2.1)
    xs.foldr op (x, [], [])
 
-/-- Nn `split₁` the `where` makes `op` visible from outside.
-    In `split`, `let` is defined only in the second equation of
-    the pattern match. `let rec` would make `op` also visible.
-
-    If `op` is not visible, in the `split_left_le` we would need
-    `lift_lets ; intro op` -/
-
 def split₁ [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
  : List a → (a × List a × List a)
  | []      => (default, [], [])
@@ -280,9 +273,63 @@ def split₁ [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
   then (x, acc.1 :: acc.2.2, acc.2.1)
   else (acc.1, x :: acc.2.2, acc.2.1)
 
+/-- In `split₁` the `where` makes `op` visible from outside.
+    In `split`, `let` is defined only in the second equation of
+    the pattern match. `let rec` would make `op` also visible.
+
+    Since `op` is not visible, we would need
+    `lift_lets ; intro op` in `split_left_le`-/
 
 theorem split_left_le [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
- (xs : List a) : (split₁ xs).2.1.length ≤ xs.length := by sorry
+    (xs : List a) : (split xs).2.1.length ≤ xs.length := by
+  cases xs with
+  | nil =>
+      simp [split]
+  | cons x xs =>
+      unfold split
+      lift_lets; intro op
+      simp
+      have h : (xs.foldr op (x, [], [])).2.1.length +
+          (xs.foldr op (x, [], [])).2.2.length = xs.length := by
+        induction xs with
+        | nil =>
+            simp
+        | cons y ys ih =>
+            simp
+            by_cases h : y ≤ (List.foldr op (x, [], []) ys).1
+            all_goals
+              simp [op, h]
+              linarith
+      omega
+/--
+  On the other hand, since `op` is visible, we can define the
+  lemma `split₁_lengths` to provide a better abstraction
+  for `split₁_left_le`.
+-/
+
+lemma split₁_lengths [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
+  (xs : List a) (x : a) :
+    (xs.foldr split₁.op (x, [], [])).2.1.length +
+    (xs.foldr split₁.op (x, [], [])).2.2.length = xs.length := by
+  induction xs with
+  | nil =>
+      simp
+  | cons y ys ih =>
+      simp
+      by_cases h : y ≤ (List.foldr split₁.op (x, [], []) ys).1
+      all_goals
+        simp [split₁.op, h]
+        linarith
+
+theorem split₁_left_le [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
+    (xs : List a) : (split₁ xs).2.1.length ≤ xs.length := by
+  cases xs with
+  | nil =>
+      simp [split₁]
+  | cons x xs =>
+      simp [split₁]
+      have h := split₁_lengths xs x
+      omega
 
 partial def mkHeap [Inhabited a] [LE a] [DecidableRel (α := a) (· ≤ ·)]
  : List a → Tree a
